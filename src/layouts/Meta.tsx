f3605dc760e8397195563b05e6072395bae0dfc6
Rajeev Singh Naruka <rajivsingh430@gmail.com>
2022-12-12 19:30:52 +0530

    fix og:image

--- src/layouts/Meta.tsx
+++ src/layouts/Meta.tsx
@@ -22,10 +22,28 @@ const Meta = (props: IMetaProps) => {
           content="width=device-width,initial-scale=1"
           key="viewport"
         />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Gaac: Github As a CDN" />
+        <meta
+          property="og:description"
+          content="A simple way to use Github as a CDN. Store your images/static files in a Github repository and use them in your projects."
+        />
         <meta
           property="og:image"
           content="https://raw.githubusercontent.com/toughyear/blog-uploads/main/uploads/gaac/landing.png"
         />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Gaac: Github As a CDN" />
+        <meta
+          name="twitter:description"
+          content="A simple way to use Github as a CDN. Store your images/static files in a Github repository and use them in your projects."
+        />
+        <meta
+          name="twitter:image"
+          content="https://raw.githubusercontent.com/toughyear/blog-uploads/main/uploads/gaac/landing.png"
+        />
+
         <link
           rel="apple-touch-icon"
           href={`${router.basePath}/apple-touch-icon.png`}
