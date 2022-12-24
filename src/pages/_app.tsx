fdeeacede12d31c2015ba270a47d85b26abac87f
Rajeev Singh Naruka <rajivsingh430@gmail.com>
2022-12-24 20:08:58 +0530

    add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/global.css';
 import 'react-toastify/dist/ReactToastify.css';
 
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import { ToastContainer } from 'react-toastify';
 
 import UserContext from '@/contexts/authContext';
@@ -11,10 +12,24 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   const user = useGitHubOAuthTokenValidity();
 
   return (
-    <UserContext.Provider value={user}>
-      <Component {...pageProps} />
-      <ToastContainer />
-    </UserContext.Provider>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={'https://www.googletagmanager.com/gtag/js?id=G-B02P0W3YVQ'}
+      />
+
+      <Script id="ga-script" strategy="lazyOnload">
+        {`  
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-B02P0W3YVQ');`}
+      </Script>
+      <UserContext.Provider value={user}>
+        <Component {...pageProps} />
+        <ToastContainer />
+      </UserContext.Provider>
+    </>
   );
 };
 
